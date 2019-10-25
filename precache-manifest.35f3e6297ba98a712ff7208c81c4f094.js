a30b8b060618e768225fecfc1e66fdde4f5ea486
Svet <stsupryk@gmail.com>
2019-10-25 05:53:33 +0300

    Updates

---
+++ precache-manifest.35f3e6297ba98a712ff7208c81c4f094.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf72c71c6f2b80b24c64e29ecce52663",
+    "url": "/klicky/index.html"
+  },
+  {
+    "revision": "fcc89f00d747e0c9dc5d",
+    "url": "/klicky/static/css/main.fdb39d53.chunk.css"
+  },
+  {
+    "revision": "52ad5bd094984b9affc7",
+    "url": "/klicky/static/js/2.c4994a12.chunk.js"
+  },
+  {
+    "revision": "fcc89f00d747e0c9dc5d",
+    "url": "/klicky/static/js/main.cc083028.chunk.js"
+  },
+  {
+    "revision": "51fba60c369ea20b05b8",
+    "url": "/klicky/static/js/runtime-main.849da923.js"
+  }
+]);
